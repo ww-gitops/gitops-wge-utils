0d6ca7ab2d653cc4d1ee70af21773f5847b98fb6
Paul Carlton <paul.carlton@weave.works>
2023-06-25 14:31:49 +0100

    Update to add updated scripts

---
+++ bin/envs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Check user is in subdirectory of their WGE GitOps Cluster repository
+# Version: 1.0
+# Author: Paul Carlton (mailto:paul.carlton@weave.works)
+
+top_level=$(git rev-parse --show-toplevel)
+if [ -f ${top_level}/.envrc ]; then
+  pushd ${top_level}
+  source .envrc
+else
+  echo "No .envrc found in ${top_level}"
+fi
